5b35f926db1e246dd0d366a10b38abaf2fe442c3
Federico <fwalovnik@gmail.com>
2020-12-14 15:33:34 -0300

    pull data statically from vue

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,33 @@
 const app = Vue.createApp({
   data() {
     return {
-      notes: [],
+      notes: [
+        {
+          text: "Hey hey hey, this note is comming from Vue. How cool is this?",
+          date: "Dec 14, 2020",
+          color: "orange",
+        },
+        {
+          text: "Hey this is another note comming from Vue",
+          date: "Dec 14, 2020",
+          color: "pink",
+        },
+        {
+          text: "There is no two without a three",
+          date: "Dec 14, 2020",
+          color: "pink",
+        },
+        {
+          text: "There is no two without a three",
+          date: "Dec 14, 2020",
+          color: "green",
+        },
+        {
+          text: "There is no two without a three",
+          date: "Dec 14, 2020",
+          color: "pink",
+        },
+      ],
       modalVisible: false,
     };
   },
