4c2cf72fd7edca1e18c3d695df996cc3fe265d02
Federico <fwalovnik@gmail.com>
2020-12-15 23:56:23 -0300

    date format modified

--- src/js/app.js
+++ src/js/app.js
@@ -62,11 +62,38 @@ const app = Vue.createApp({
       this.toggleModal();
     },
     formatDate(now) {
-      const month = now.getMonth();
-      const day = now.getDay();
+      let month = now.getMonth() + 1;
+      const day = now.getDate();
       const year = now.getFullYear();
 
-      let formatedDate = `${month + 1}-${day}-${year}`;
+      //God of programming forgive me for doing this, I promise that I will learn to do it in a better way.
+      if (month === 1) {
+        month = "Jan";
+      } else if (month === 2) {
+        month = "Feb";
+      } else if (month === 3) {
+        month = "Mar";
+      } else if (month === 4) {
+        month = "Apr";
+      } else if (month === 5) {
+        month = "May";
+      } else if (month === 6) {
+        month = "Jun";
+      } else if (month === 7) {
+        month = "Jul";
+      } else if (month === 8) {
+        month = "Aug";
+      } else if (month === 9) {
+        month = "Sep";
+      } else if (month === 10) {
+        month = "Oct";
+      } else if (month === 11) {
+        month = "Nov";
+      } else if (month === 12) {
+        month = "Dec";
+      }
+
+      let formatedDate = `${month} ${day}, ${year}`;
       return formatedDate;
     },
   },
